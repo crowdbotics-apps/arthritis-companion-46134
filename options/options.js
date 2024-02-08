0ff2ba319beb5f1b9b69a293e29031e088cb9b3e
Crowdbotics <team@crowdbotics.com>
2024-02-08 17:03:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "arthritis-companion-46134",
+  url: "https://arthritis-companion-46134.botics.co",
+  api: "https://arthritis-companion-46134.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
